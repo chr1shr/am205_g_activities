07fb1b2ba6a958c2eddd8d825559c685089a2246
Jovana Andrejevic <jandrejevic@g.harvard.edu>
2021-10-08 19:35:50 -0400

    Add simple linear advection example exhibiting oscillations.

--- eno_methods/README.md
+++ eno_methods/README.md
@@ -2,4 +2,6 @@
 This git repository contains Python demos and jupyter notebook exercises featured during the activity.
 
 - ```discont_inter.py``` - Example demonstrating Gibbs phenomenon when constructing a polynomial interpolant of an approximately discontinuous function.
+-```lin_adv.py``` - Linear advection example with initial sigmoid step function that develops oscillations using a 2nd order upwind scheme.
+
 
---
+++ eno_methods/lin_adv.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+import numpy as np
+import matplotlib.pyplot as plt
+from math import *
+
+# Linear advection example on [-1,1] using 2nd order upwinding.
+
+# Grid size
+m=200
+mp2=m+2 # pad with 2 ghost nodes
+# PDE-related constants.
+c=-1.
+dx=2.0/(m-1)
+dt=0.05*dx
+nu=c*dt/dx
+T=1. # final simulation time
+snaps=10 # number of snapshots to output (excluding t=0)
+iters=int(T/dt)//snaps # iterations to perform between snapshots
+
+u=np.empty(mp2) # memory for the current step
+u1=np.empty(mp2) # memory for the next step
+us=np.empty((m,snaps+1)) # memory for all snapshots
+
+# Initial condition
+k=100
+for j in range(mp2):
+    x=-1+dx*j
+    u[j]=1./(1+np.exp(-k*x)) # smooth function
+u1=np.copy(u)
+us[:,0]=u[:m]
+
+# Integrate the PDE
+for i in range(1,snaps+1):
+    for k in range(iters):
+        for j in range(m):
+            u1[j]=u[j]-0.5*nu*(-u[j+2]+4*u[j+1]-3*u[j]) # 2nd order upwinding scheme
+        u=np.copy(u1)
+    us[:,i]=u[:m]
+
+# Output results
+fig, ax = plt.subplots(1,1,figsize=(6,3))
+x=np.linspace(-1,1,m)
+cmap=plt.cm.gnuplot
+for i in range(snaps+1):
+    ax.plot(x,us[:,i],color=cmap(i*iters*dt),alpha=0.8)
+ax.set_ylim(-0.5,1.5)
+ax.set_xlabel('x')
+ax.set_ylabel('u')
+fig.tight_layout()
+plt.show()
